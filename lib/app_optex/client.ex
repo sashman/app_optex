78db29aa98d793a92c814b27ed17e9aad2ae9732
sashman <sashman90@gmail.com>
2019-04-07 23:35:06 +0100

    Doc update #2

--- lib/app_optex/client.ex
+++ lib/app_optex/client.ex
@@ -28,7 +28,7 @@ defmodule AppOptex.Client do
 
   @doc """
 
-  Read from [AppOptics retrieve measurement API](https://docs.appoptics.com/api/?shell#retrieve-a-measurement) given a query.
+  Read from [AppOptics retrieve measurement API](https://docs.appoptics.com/api/?shell#retrieve-a-measurement) given a query. Returns the AppOptex API response.
 
   * appoptics_url - AppOptics API endpoint.
   * token - AppOptics auth token.
@@ -41,8 +41,19 @@ defmodule AppOptex.Client do
 
   ## Examples
 
-      iex> AppOptex.Client.send_measurements("https://api.appoptics.com/v1/measurements", "MY_TOKEN", [%{name: "my.mertric.name", value: 1}], %{"name" => "value"})
-      {:ok, %HTTPoison.Response{}}
+      iex> AppOptex.Client.read_measurements("https://api.appoptics.com/v1/measurements", "MY_TOKEN", "my.other_mertic", 60, %{duration: 999999})
+      %{
+            "attributes" => %{"created_by_ua" => "hackney/1.15.1"},
+            "links" => [],
+            "name" => "my.other_mertic",
+            "resolution" => 60,
+            "series" => [
+                %{
+                "measurements" => [%{"time" => 1554667320, "value" => 5.0}],
+                "tags" => %{"my_tag" => "value"}
+                }
+            ]
+        }
 
   """
 
