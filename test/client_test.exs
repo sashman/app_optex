01355bc267330773ee32ccbed9e456a238ddc717
sashman <sashman90@gmail.com>
2019-04-07 23:54:25 +0100

    Test for happy path reading measurements #2

--- test/client_test.exs
+++ test/client_test.exs
@@ -27,4 +27,29 @@ defmodule AppOptex.ClientTest do
       assert result == {:ok, %HTTPoison.Response{}}
     end
   end
+
+  test "read_measurements/5 makes a call with correct payload" do
+    appoptics_url = "url"
+    token = "token"
+    metric_name = "my.metric"
+    resolution = 60
+
+    params = %{
+      start_time: 1_554_676_958,
+      end_time: 1_554_676_999
+    }
+
+    response = %{body: Poison.encode!(%{response: "from AppOptics"})}
+
+    with_mock HTTPoison,
+      get!: fn "url/my.metric" <> _,
+               [{"Content-Type", "application/json"}],
+               hackney: [basic_auth: {^token, ""}] ->
+        response
+      end do
+      result = Client.read_measurements(appoptics_url, token, metric_name, resolution, params)
+
+      assert result == response.body |> Poison.decode!()
+    end
+  end
 end
