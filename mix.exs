0e195c7e5bf1eb4e4f62c399ef16c51de275a296
sashman <sashman90@gmail.com>
2019-04-08 15:13:54 +0100

    Adding ex docs

--- mix.exs
+++ mix.exs
@@ -24,9 +24,8 @@ defmodule AppOptex.MixProject do
     [
       {:httpoison, "~> 1.4"},
       {:poison, "~> 3.1"},
-      {:mock, "~> 0.3.0", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:mock, "~> 0.3.0", only: :test},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
     ]
   end
 end
